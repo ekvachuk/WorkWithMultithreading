c1278971609bacad2a3bab13895501cf7bac364d
ekvachuk <jekvach@ukr.net>
2019-07-03 21:19:43 +0300

    Add MultiRunner

--- src/org/itstep/AppRunner.java
+++ src/org/itstep/AppRunner.java
@@ -22,16 +22,8 @@ public class AppRunner {
 //        System.out.println(date);
 
         for (int i = 0; i < 10; i++) {
-            ConnectionData connectionData = new ConnectionData();
-            connectionData.setSessionId(Randomizer.getRandomNumber(10_000_000, 99_999_999));
-            connectionData.setUserLogin(Randomizer.getRandomString(10));
-            connectionData.setTime(new Date().getTime() - Randomizer.getRandomNumber(0, 1000 * 60 * 60 * 24));
-            connectionData.setIp(Randomizer.getRandomNumber(100, 255) + "." +
-                    Randomizer.getRandomNumber(100, 255) + "." +
-                    Randomizer.getRandomNumber(100, 255) + "." +
-                    Randomizer.getRandomNumber(100, 255));
-            FileManagerservice.writeDataToFile(connectionData, true);
-
+            AppMultiRunner appMultiRunner = new AppMultiRunner();
+            appMultiRunner.start();
         }
     }
 }
